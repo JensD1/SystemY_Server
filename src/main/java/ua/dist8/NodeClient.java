a44841c263259c181bed2f75afab8b3346426721
unknown <nader.motee@gmail.com>
2020-04-17 15:53:39 +0100

    Multicast

--- src/main/java/ua/dist8/NodeClient.java
+++ src/main/java/ua/dist8/NodeClient.java
@@ -1,7 +1,60 @@
 package ua.dist8;
 
+import jdk.nashorn.internal.parser.JSONParser;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.net.*;
+
 public class NodeClient {
 
+    private static InetAddress MCgroup;
+
+    static {
+        try {
+            MCgroup = InetAddress.getByName("192.1.1.69");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        JSONObject obj = new JSONObject();
+        Hashing h = new Hashing();
+        obj.put("name", "node1");
+        obj.put("ip", MCgroup);
+        MulticastSocket ms = new MulticastSocket(6012);
+
+        ms.joinGroup(MCgroup);
+        byte[] contents = obj.toString().getBytes();
+        DatagramPacket packet = new DatagramPacket(contents,contents.length, MCgroup, 6012);
+        ms.send(packet);
+        //ms.leaveGroup(ms.getLocalSocketAddress(), NetworkInterface.getByInetAddress(group));
+
+
+    }
+
+    public void receiveMC () throws IOException {
+        MulticastSocket ms = new MulticastSocket(6012);
+        ms.joinGroup(MCgroup);
+        while(true){
+            byte[] buf = new byte[1000];
+            DatagramPacket recv = new DatagramPacket(buf, buf.length);
+            ms.receive(recv);
+            if (recv.getLength()>0){
+
+                String s = new String(String.valueOf(recv));
+                JSONParser parser = new JSONParser();
+                JSONObject json = (JSONObject) parser.parse(stringToParse);
+
+
+                receivedmulticast(recv);
+
+            }
+
+        }
+
+    }
 
 
 }
