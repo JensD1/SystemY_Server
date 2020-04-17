7efdc222c6d8c3ba98a2ca2902a8490860b8d3af
Odemar <odemar.martens@outlook.com>
2020-04-17 18:33:20 +0200

    started run implementation of MultcastHandlerThread

--- src/main/java/ua/dist8/MulticastHandlerThread.java
+++ src/main/java/ua/dist8/MulticastHandlerThread.java
@@ -1,4 +1,37 @@
 package ua.dist8;
 
-public class MulticastHandlerThread {
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+
+// verwerk multicast udp pakket
+public class MulticastHandlerThread extends Thread{
+    Hashing hash;
+    DatagramPacket datagramPacket;
+
+    public MulticastHandlerThread(DatagramPacket datagramPacket){
+        this.datagramPacket = datagramPacket;
+        this.hash = new Hashing();
+    }
+
+    public void run() {
+        try {
+            InetAddress clientAddress = datagramPacket.getAddress();
+            String clientHostName = clientAddress.getHostName();
+            Integer hashingValue = hash.createHash(clientHostName);
+            // kijk of deze hash in de treemap zit => treemap inladen vanuit file (gecontroleerd via semaphores)
+            //int numberOfNodes = addNode()
+            //if( numberOfNodes >= 0){
+            // send number of nodes in network
+            //}
+            //else{
+            // send -1 so client knows there was a problem
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+
+    }
 }
+
+
--- src/main/java/ua/dist8/NamingServerApplication.java
+++ src/main/java/ua/dist8/NamingServerApplication.java
@@ -3,9 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.*;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 import java.lang.Runnable;
 
 @SpringBootApplication
@@ -13,14 +11,16 @@ public class NamingServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(NamingServerApplication.class, args);
         try{
-            ServerSocket serverSocket = new ServerSocket(5000);
-            int counter=0;
+            byte[] receivedContent = null;
+            int size = 10000;
+            DatagramPacket receivedPacket = new DatagramPacket(receivedContent, size);
+            DatagramSocket serverSocket = new DatagramSocket(1234);
             System.out.println("Server Started ....");
+
             while(true){
-                counter++;
-                Socket clientSocket=serverSocket.accept();  //server accept the client connection request
-                System.out.println(" >> " + "Client No:" + counter + " started!");
-                MulitcastHandlerThread sct = new TcpServerClientThread(clientSocket,counter); //send  the request to a separate thread
+                serverSocket.receive(receivedPacket); //server accept the client connection request
+
+                MulticastHandlerThread sct = new MulticastHandlerThread(receivedPacket); //send  the request to a separate thread
                 sct.start();
             }
         }catch(Exception e){
