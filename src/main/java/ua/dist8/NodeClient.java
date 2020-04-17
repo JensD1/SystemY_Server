9fdf2def89aedce9787989897fca4df09ad47053
JensD1 <jens.duym@student.uantwerpen.be>
2020-04-17 13:36:32 +0200

    Initialisation of namingsserver's boodstrap function

--- src/main/java/ua/dist8/NodeClient.java
+++ src/main/java/ua/dist8/NodeClient.java
@@ -1,7 +1,28 @@
 package ua.dist8;
 
+import java.util.HashMap;
+
 public class NodeClient {
 
+    public int receiveUnicastMessage(){
+        Integer receivedNumberOfMessages = 0;
+        Boolean leaveWhile = Boolean.FALSE;
+        do{
+            if( reveivedMessage){
+                //readmessage
+                receivedNumberOfMessages++;
+                if(vanNameServer && aantalNodes <= 0){
+                    leaveWhile = Boolean.TRUE; // er is maar 1 bericht dat ontvangen moest worden en dit is ontvangen
+                }
+                if(vanNode){
+                    // handel dit verder af.
+                }
+            }
+        }while(!leaveWhile && receivedNumberOfMessages<3);
+        return 0;
+    }
 
-
+    public int sendUnicastMessage(){
+        return 0;
+    }
 }
