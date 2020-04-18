9c747ee9681a39650693c730aad4b225e21a6b37
Odemar <odemar.martens@outlook.com>
2020-04-18 16:53:07 +0200

    updated UDP broadcast Listner

--- src/main/java/ua/dist8/UDPListener.java
+++ src/main/java/ua/dist8/UDPListener.java
@@ -1,24 +1,26 @@
 package ua.dist8;
 
 import java.net.DatagramPacket;
-import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 
 public class UDPListener extends Thread {
     public void run() {
         try {
             // UDP parameters
-            System.out.println("Initializing UDP listener...");
-            byte[] receivedContent = new byte[10000];
-            int size = 10000;
-            DatagramPacket receivedPacket = new DatagramPacket(receivedContent, size);
-            DatagramSocket serverSocket = new DatagramSocket(1234);
+            System.out.println("Initializing UDP listener..." );
+            MulticastSocket ms = new MulticastSocket(6012);
+            InetAddress MCgroup = InetAddress.getByName("224.0.0.200");
+            ms.joinGroup(MCgroup);
+            System.out.println("Listening on Multicast address 224.0.0.200");
 
 
             while (true) {
-                System.out.println("Listening for UDP messages on port 1234..");
-                serverSocket.receive(receivedPacket); //server accept the client connection request
+                byte[] buf = new byte[1000];
+                DatagramPacket datagramPacket = new DatagramPacket(buf, buf.length);
+                ms.receive(datagramPacket);
                 System.out.println("Packet received! Creating new thread to process the request.");
-                UDPHandlerThread thread = new UDPHandlerThread(receivedPacket); //send  the request to a separate thread
+                UDPHandlerThread thread = new UDPHandlerThread(datagramPacket); //send  the request to a separate thread
                 thread.start();
             }
         } catch (Exception e) {
