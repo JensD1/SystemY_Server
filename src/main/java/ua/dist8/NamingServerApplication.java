fe9d2a3592f82d6d5e1b49337fed94a0d93c3123
Odemar <odemar.martens@outlook.com>
2020-04-18 13:55:23 +0200

    added methods from NetworkHashMap and fixed library issues

--- src/main/java/ua/dist8/NamingServerApplication.java
+++ src/main/java/ua/dist8/NamingServerApplication.java
@@ -10,16 +10,18 @@ import java.net.*;
 public class NamingServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(NamingServerApplication.class, args);
-        try{
+        System.out.println("Server Started ....");
+       try{
             // UDP parameters
             byte[] receivedContent = null;
             int size = 10000;
             DatagramPacket receivedPacket = new DatagramPacket(receivedContent, size);
             DatagramSocket serverSocket = new DatagramSocket(1234);
-            System.out.println("Server Started ....");
+
 
             // Server listens on port 1234
             while(true){
+                System.out.println("Listening on port 1234..");
                 serverSocket.receive(receivedPacket); //server accept the client connection request
 
                 MulticastHandlerThread thread = new MulticastHandlerThread(receivedPacket); //send  the request to a separate thread
